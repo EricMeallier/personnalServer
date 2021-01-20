4139bac682cffcd1051d476d5fcf9f416a3b44b6
Eric Meallier <emeallier@sada.fr>
2021-01-20 13:04:12 +0100

    Ends all cron/timed job migration to systemd

--- roles/backup/files/backup.sh
+++ roles/backup/files/backup.sh
@@ -9,34 +9,41 @@ auth=$(curl -s "https://api.pcloud.com/userinfo?getauth=1&username=eric@meallier
 ## gogs
 tar zcvf ${targetDir}/gogs-${timestamp=}-data.tar.gz /data/gogs-repositories
 curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/gogs-${timestamp=}-data.tar.gz
+rm -f ${targetDir}/gogs-${timestamp=}-data.tar.gz
 
 sudo su - postgres -c "pg_dump gogs > ${targetDir}/gogs-${timestamp=}.dmp"
 gzip -f ${targetDir}/gogs-${timestamp=}.dmp
 curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/gogs-${timestamp=}.dmp.gz
-
+rm -f ${targetDir}/gogs-${timestamp=}.dmp.gz
 
 # redmine
 tar zcvf ${targetDir}/redmine-${timestamp=}-files.tar.gz /data/redmine-files
 curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/redmine-${timestamp=}-files.tar.gz
+rm -f ${targetDir}/redmine-${timestamp=}-files.tar.gz
 
 sudo su - postgres -c "pg_dump redmine > ${targetDir}/redmine-${timestamp=}.dmp"
 gzip -f ${targetDir}/redmine-${timestamp=}.dmp
 curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/redmine-${timestamp=}.dmp.gz
+rm -f ${targetDir}/redmine-${timestamp=}.dmp.gz
 
 # nextcloud
 tar zcvf ${targetDir}/nextcloud-${timestamp=}-data.tgz /data/nextcloud
 curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/nextcloud-${timestamp=}-data.tgz
+rm -f ${targetDir}/nextcloud-${timestamp=}-data.tgz
 
 sudo su - postgres -c "pg_dump nextcloud > ${targetDir}/nextcloud-${timestamp=}.dmp"
 gzip -f ${targetDir}/nextcloud-${timestamp=}.dmp
 curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/nextcloud-${timestamp=}.dmp.gz
+rm -f ${targetDir}/nextcloud-${timestamp=}.dmp.gz
 
 # etherpad
 sudo su - postgres -c "pg_dump etherpad > ${targetDir}/etherpad-${timestamp=}.dmp"
 gzip -f ${targetDir}/etherpad-${timestamp=}.dmp
 curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/etherpad-${timestamp=}.dmp.gz
+rm -f ${targetDir}/etherpad-${timestamp=}.dmp.gz
 
 # ethercalc
 cp /var/lib/redis/dump.rdb ${targetDir}/ethercalc-${timestamp=}.rdb
 gzip -f ${targetDir}/ethercalc-${timestamp=}.rdb
 curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/ethercalc-${timestamp=}.rdb.gz
+rm -f ${targetDir}/ethercalc-${timestamp=}.rdb.gz
---
+++ roles/nextcloud/files/nextcloudTrash.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cd /opt/nextcloud; sudo -u nobody /usr/bin/php7.3 occ trashbin:cleanup rickou_412
