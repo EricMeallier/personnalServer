1a604fc3ad334f8c7e740a55269fd975a6f29588
rickou_412 <eric.meallier@gmail.com>
2021-07-13 15:40:55 +0200

    Add 'documentation/nextcloudClient.update.sh'

---
+++ documentation/nextcloudClient.update.sh
@@ -0,0 +1,8 @@
+export NEW_VERSION=3.2.4
+
+sudo rm -f /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
+
+sudo mv ~/Downloads/Nextcloud-${NEW_VERSION}-x86_64.AppImage /opt
+sudo chmod a+x /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
+sudo rm -f /opt/Nextcloud
+sudo ln -s /opt/Nextcloud-3.2.4-x86_64.AppImage /opt/Nextcloud
