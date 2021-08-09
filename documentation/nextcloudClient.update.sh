d27edea499321028111203936116effce1a41da5
rickou_412 <eric.meallier@gmail.com>
2021-08-09 13:57:22 +0200

    Update 'documentation/nextcloudClient.update.sh'

--- documentation/nextcloudClient.update.sh
+++ documentation/nextcloudClient.update.sh
@@ -5,4 +5,4 @@ sudo rm -f /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
 sudo mv ~/Downloads/Nextcloud-${NEW_VERSION}-x86_64.AppImage /opt
 sudo chmod a+x /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
 sudo rm -f /opt/Nextcloud
-sudo ln -s /opt/Nextcloud-3.2.4-x86_64.AppImage /opt/Nextcloud
+sudo ln -s /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage /opt/Nextcloud
