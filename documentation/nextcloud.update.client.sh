ae841a4024ad0a4392be68662f4c8c78adea7909
rickou_412 <eric.meallier@gmail.com>
2022-07-06 10:36:51 +0200

    update nextcloud client to 3.5.2

--- documentation/nextcloud.update.client.sh
+++ documentation/nextcloud.update.client.sh
@@ -1,8 +1,8 @@
-export NEW_VERSION=3.5.1
+export NEW_VERSION=3.5.2
 
 sudo rm -f /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
 
-sudo mv ~/Downloads/Nextcloud-${NEW_VERSION}-x86_64.AppImage /opt
+sudo curl -L https://github.com/nextcloud/desktop/releases/download/v${NEW_VERSION}/Nextcloud-${NEW_VERSION}-x86_64.AppImage -o /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
 sudo chmod a+x /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
 sudo rm -f /opt/Nextcloud
 sudo ln -s /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage /opt/Nextcloud
