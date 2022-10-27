55631203ee66e4a5646a4b0bd499a2aec5f5a98a
Eric Meallier <emeallier@sada.fr>
2022-10-27 18:11:04 +0200

    fix backup script for europe cloud

--- roles/backup/templates/backup.sh
+++ roles/backup/templates/backup.sh
@@ -4,61 +4,58 @@ timestamp=`date +%Y%m%d_%H%M`
 timestamp='last'
 targetDir='/backup'
 
-auth=$(curl -s "https://api.pcloud.com/userinfo?getauth=1&username={{ pcloud.username }}&password={{ pcloud.password }}" | jq -r '.auth')
+auth=$(curl -s "https://eapi.pcloud.com/userinfo?getauth=1&username={{ pcloud.username }}&password={{ pcloud.password }}" | jq -r '.auth')
 
 systemctl stop nginx
 systemctl stop etherpad
 systemctl stop ethercalc
 systemctl stop gogs
-systemctl stop jicofo.service
-systemctl stop jitsi-videobridge2.service
-systemctl stop prosody.service
 
 ## gogs
 tar zcvf ${targetDir}/gogs-${timestamp=}-data.tar.gz /data/gogs-repositories
-curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/gogs-${timestamp=}-data.tar.gz
+curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/gogs-${timestamp=}-data.tar.gz
 rm -f ${targetDir}/gogs-${timestamp=}-data.tar.gz
 
 sudo su - postgres -c "pg_dump gogs > ${targetDir}/gogs-${timestamp=}.dmp"
 gzip -f ${targetDir}/gogs-${timestamp=}.dmp
-curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/gogs-${timestamp=}.dmp.gz
+curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/gogs-${timestamp=}.dmp.gz
 rm -f ${targetDir}/gogs-${timestamp=}.dmp.gz
 
 # redmine
 tar zcvf ${targetDir}/redmine-${timestamp=}-files.tar.gz /data/redmine-files
-curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/redmine-${timestamp=}-files.tar.gz
+curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/redmine-${timestamp=}-files.tar.gz
 rm -f ${targetDir}/redmine-${timestamp=}-files.tar.gz
 
 sudo su - postgres -c "pg_dump redmine > ${targetDir}/redmine-${timestamp=}.dmp"
 gzip -f ${targetDir}/redmine-${timestamp=}.dmp
-curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/redmine-${timestamp=}.dmp.gz
+curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/redmine-${timestamp=}.dmp.gz
 rm -f ${targetDir}/redmine-${timestamp=}.dmp.gz
 
 # nextcloud
-GZIP=-9; tar zcvf ${targetDir}/nextcloud-${timestamp=}-data.tgz --exclude={"nextcloud.log*","*A_Trier"} /data/nextcloud
-curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/nextcloud-${timestamp=}-data.tgz
+GZIP=-9; tar zcvf ${targetDir}/nextcloud-${timestamp=}-data.tgz --exclude={"nextcloud.log*","*A_Trier","*BackupPC"} /data/nextcloud
+curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/nextcloud-${timestamp=}-data.tgz
 rm -f ${targetDir}/nextcloud-${timestamp=}-data.tgz
 
 sudo su - postgres -c "pg_dump nextcloud > ${targetDir}/nextcloud-${timestamp=}.dmp"
 gzip -f ${targetDir}/nextcloud-${timestamp=}.dmp
-curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/nextcloud-${timestamp=}.dmp.gz
+curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/nextcloud-${timestamp=}.dmp.gz
 rm -f ${targetDir}/nextcloud-${timestamp=}.dmp.gz
 
 # etherpad
 sudo su - postgres -c "pg_dump etherpad > ${targetDir}/etherpad-${timestamp=}.dmp"
 gzip -f ${targetDir}/etherpad-${timestamp=}.dmp
-curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/etherpad-${timestamp=}.dmp.gz
+curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/etherpad-${timestamp=}.dmp.gz
 rm -f ${targetDir}/etherpad-${timestamp=}.dmp.gz
 
 # ethercalc
 cp /var/lib/redis/dump.rdb ${targetDir}/ethercalc-${timestamp=}.rdb
 gzip -f ${targetDir}/ethercalc-${timestamp=}.rdb
-curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/ethercalc-${timestamp=}.rdb.gz
+curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/ethercalc-${timestamp=}.rdb.gz
 rm -f ${targetDir}/ethercalc-${timestamp=}.rdb.gz
 
 # picocms
 tar zcvf ${targetDir}/picocms-${timestamp=}-data.tgz /data/picocms
-curl -X POST "https://api.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/picocms-${timestamp=}-data.tgz
+curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/picocms-${timestamp=}-data.tgz
 rm -f ${targetDir}/picocms-${timestamp=}-data.tgz
 
 # restart
@@ -66,7 +63,4 @@ systemctl restart postgresql
 systemctl start etherpad
 systemctl start ethercalc
 systemctl start gogs
-systemctl start jicofo.service
-systemctl start jitsi-videobridge2.service
-systemctl start prosody.service
 systemctl start nginx
