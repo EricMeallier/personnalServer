ee997b369fa932da73c3ce3ca75865ba0c024dcb
Eric Meallier <emeallier@sada.fr>
2023-02-28 15:10:55 +0100

    fix terraform script

---
+++ buildAllVultr.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+dir=$(cd -P -- "$(dirname -- "$BASH_SOURCE[0]")" && pwd -P)
+
+#
+# load utilities
+#
+_utilities="${dir}/utils.sh"
+if [ ! -r "${_utilities}" ]; then
+  echo "Failed to read file ${_utilities}"
+  exit 1
+fi
+. "${_utilities}"
+
+
+export UseVagrant='testing'
+
+cd ${dir}/terraform
+terraform apply -auto-approve -var "vultr_api_key=${VULTR_API_KEY}"
+cd ${dir}
+./bootstrap.sh
+./launchPlaybook.sh
+
+displayDuration
---
+++ destroyAllVultr.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+dir=$(cd -P -- "$(dirname -- "$BASH_SOURCE[0]")" && pwd -P)
+
+#
+# load utilities
+#
+_utilities="${dir}/utils.sh"
+if [ ! -r "${_utilities}" ]; then
+  echo "Failed to read file ${_utilities}"
+  exit 1
+fi
+. "${_utilities}"
+
+
+export UseVagrant='testing'
+
+cd ${dir}/terraform
+terraform destroy -auto-approve -var "vultr_api_key=${VULTR_API_KEY}"
+
+displayDuration
