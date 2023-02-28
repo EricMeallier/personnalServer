988b9ec9aeb559a17e2f423f6d5c0a34af11825c
Eric Meallier <emeallier@sada.fr>
2023-02-28 14:57:29 +0100

    adding terraform + inventory generation

---
+++ terrafrom/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    vultr = {
+      source = "vultr/vultr"
+      version = "2.12.1"
+    }
+  }
+}
+
+# Configure the Vultr Provider
+provider "vultr" {
+  api_key = var.vultr_api_key
+  rate_limit = 100
+  retry_limit = 3
+}
+
+resource "vultr_ssh_key" "my_user" {
+  name = "Root SSH key"
+  ssh_key = "${file("~/.ssh/ovhuser.pub")}"
+}
+
+resource "vultr_instance" "my_instance" {
+    plan = "vc2-1c-2gb"
+    region = "cdg"
+    os_id = 477
+    enable_ipv6 = false
+    ssh_key_ids = ["${vultr_ssh_key.my_user.id}"]
+    script_id = vultr_startup_script.bootinit.id
+}
+
+resource "vultr_startup_script" "bootinit" {
+    name = "ansible-deploy"
+    script = filebase64("bootinit.sh")
+    type = "boot"
+}
+	
+resource "local_file" "ansible_inventory" {
+  content = templatefile("inventory.tmpl",
+    {
+     main_ip = vultr_instance.my_instance.main_ip
+    }
+  )
+  filename = "../inventory/inventoryTerraform"
+}
---
+++ terrafrom/terraform.template.tfvars
@@ -0,0 +1,2 @@
+key_name        = "terraformEricKey"
+public_key_path = "~/.ssh/terraform.pub"
