9b1cccb34d3ef3cdf1007f99ef94808bdb6a900f
Meallier Eric <emeallier@sada.fr>
2023-12-11 14:57:15 +0100

    automate nextcloud version restart

--- documentation/nextcloud.update.client.sh
+++ documentation/nextcloud.update.client.sh
@@ -1,4 +1,4 @@
-export NEW_VERSION=3.10.1
+export NEW_VERSION=3.10.2
 
 sudo rm -f /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
 
@@ -6,3 +6,7 @@ sudo curl -L https://github.com/nextcloud-releases/desktop/releases/download/v${
 sudo chmod a+x /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
 sudo rm -f /opt/Nextcloud
 sudo ln -s /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage /opt/Nextcloud
+
+ps -ef | grep '\/opt\/Nextcloud' | awk '{print $2}' | xargs kill
+
+/opt/Nextcloud > /dev/null 2>&1&
