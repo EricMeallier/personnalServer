74fbb267429ea50d0035843ca9f1e2b753ad41bb
Meallier Eric <emeallier@sada.fr>
2024-03-09 10:32:30 +0100

    nextcloud client automatic update

--- documentation/nextcloud.update.client.sh
+++ documentation/nextcloud.update.client.sh
@@ -1,10 +1,9 @@
-export NEW_VERSION=3.12.0
 
-sudo rm -f /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
+NEW_VERSION=`{ curl -sL https://github.com/nextcloud/desktop/releases ; curl -sL https://github.com/nextcloud/desktop/releases?page=2 ; } | grep "tag/v[0-9]*\.[0-9]*\.[0-9]*\"" | sed -e "s/.*tag\/v\([0-9]*\.[0-9]*\.[0-9]*\).*/\1/" | sort --version-sort | tail -1`
+sudo rm -f /opt/Nextcloud*
 
-sudo curl -L https://github.com/nextcloud-releases/desktop/releases/download/v${NEW_VERSION}/Nextcloud-${NEW_VERSION}-x86_64.AppImage -o /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
+sudo curl -sL https://github.com/nextcloud-releases/desktop/releases/download/v${NEW_VERSION}/Nextcloud-${NEW_VERSION}-x86_64.AppImage -o /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
 sudo chmod a+x /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
-sudo rm -f /opt/Nextcloud
 sudo ln -s /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage /opt/Nextcloud
 
 ps -ef | grep '\/opt\/Nextcloud' | awk '{print $2}' | xargs kill
