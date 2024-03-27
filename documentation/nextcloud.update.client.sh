9445247f31b05abafcfdd2e4d0be364feb6de996
Meallier Eric <emeallier@sada.fr>
2024-03-27 18:44:36 +0100

    nextcloud client: Print new installed version

--- documentation/nextcloud.update.client.sh
+++ documentation/nextcloud.update.client.sh
@@ -1,5 +1,6 @@
 
 NEW_VERSION=`{ curl -sL https://github.com/nextcloud/desktop/releases ; curl -sL https://github.com/nextcloud/desktop/releases?page=2 ; } | grep "tag/v[0-9]*\.[0-9]*\.[0-9]*\"" | sed -e "s/.*tag\/v\([0-9]*\.[0-9]*\.[0-9]*\).*/\1/" | sort --version-sort | tail -1`
+echo "Upgrade to $NEW_VERSION"
 sudo rm -f /opt/Nextcloud*
 
 sudo curl -sL https://github.com/nextcloud-releases/desktop/releases/download/v${NEW_VERSION}/Nextcloud-${NEW_VERSION}-x86_64.AppImage -o /opt/Nextcloud-${NEW_VERSION}-x86_64.AppImage
