480dbd81596bcdeaff688ab812b8af6713d3241e
Meallier Eric <eric.meallier@free..fr>
2024-09-11 16:38:14 +0200

    fix vagrant inventory

--- inventory/vagrantList.sh
+++ inventory/vagrantList.sh
@@ -2,15 +2,31 @@
 
 dir=$(cd -P -- "$(dirname -- "$BASH_SOURCE[0]")" && pwd -P)
 
-vagrant ssh-config > /dev/null 2>&1
+
+# User for ssh connection
+defaultUser=ovhuser
+defaultIdentity=~/.ssh/ovhuser
+
+vagrantSshConfig=$(mktemp)
+trap 'rm -f "${vagrant_ssh_config_XXXX}"' EXIT
+
+
+cd ${dir}/../provision/vm;vagrant ssh-config > ${vagrantSshConfig} 2>&1
 if [ $? -eq 0 ]
 then
 
-    vagrantIP=`cd ${dir}/../provision/vm;vagrant ssh-config | grep HostName |awk  '{print $2}'`
-    vagrantPort=`cd ${dir}/../provision/vm;vagrant ssh-config | grep Port |awk  '{print $2}'`
-    vagrantUser=`cd ${dir}/../provision/vm;vagrant ssh-config | grep 'User ' |awk  '{print $2}'`
-    vagrantIdentity=`cd ${dir}/../provision/vm;vagrant ssh-config | grep IdentityFile |awk  '{print $2}'`
+    vagrantIP=`cat ${vagrantSshConfig} | grep HostName |awk  '{print $2}'`
+    vagrantPort=`cat ${vagrantSshConfig} | grep Port |awk  '{print $2}'`
 
+    ps -f | grep bootstrapVMAuthent >/dev/null 2>&1
+    if [ $? -eq 0 ]
+    then
+        vagrantUser=`cat ${vagrantSshConfig} | grep 'User ' |awk  '{print $2}'`
+        vagrantIdentity=`cat ${vagrantSshConfig} | grep IdentityFile |awk  '{print $2}'`
+    else
+        vagrantUser=${defaultUser}
+        vagrantIdentity=${defaultIdentity}
+    fi
 
     # inventory building
     printf "{\"vagrantHosts\":{\"hosts\":[\"vagrant\"]},"
@@ -20,6 +36,7 @@ then
     printf "\"vagrant\":"
     printf "{\"ansible_host\":\"${vagrantIP}\","
     printf "\"ansible_port\":\"${vagrantPort}\","
+
     printf "\"ansible_user\":\"${vagrantUser}\","
     printf "\"ansible_ssh_private_key_file\":\"${vagrantIdentity}\" }"
 
