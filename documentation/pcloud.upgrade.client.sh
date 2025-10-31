006bf2aa080dd28cae3a0a2a5a08f6086e0ba8e7
EricMeallier <eric.meallier@free..fr>
2025-10-31 10:41:56 +0100

    enhance pcloud client upgrade script

--- documentation/pcloud.upgrade.client.sh
+++ documentation/pcloud.upgrade.client.sh
@@ -4,6 +4,7 @@ sudo rm -f /opt/pcloud
 PCLOUD_CLIENT_VERSION=`curl -s https://www.pcloud.com/release-notes/linux.html | grep '<b>[0-9]*\.[0-9]*\.[0-9]*<\/b>' | grep '20[2-9][0-9])' | sed -e 's/.*\([0-9]\.[0-9]*\.[0-9]*\).*/\1/' | sort --version-sort | tail -1`
 PCLOUD_CLIENT_LANDING_PAGE=`curl -s https://www.pcloud.com/release-notes/linux.html | grep "$PCLOUD_CLIENT_VERSION" | sed -e 's/.*a href="\(.*\)" .*/\1/'`
 
+echo "Upgrade to $PCLOUD_CLIENT_VERSION"
 PCLOUD_CLIENT_URL=`curl -s $PCLOUD_CLIENT_LANDING_PAGE | grep downloadlink | sed -e 's/.*downloadlink": "\(.*\)".*/\1/' | tr -d '\\'`
 
 curl -s $PCLOUD_CLIENT_URL -o /tmp/pcloud
