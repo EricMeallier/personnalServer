df39d210ea7335793f2b3b825a3f312a7acd8cd6
EricMeallier <eric.meallier@free..fr>
2025-12-18 11:25:44 +0100

    enhance backup script

--- roles/backup/templates/backup.sh
+++ roles/backup/templates/backup.sh
@@ -7,11 +7,12 @@ targetDir='/backup'
 auth=$(curl -s "https://eapi.pcloud.com/userinfo?getauth=1&username={{ pcloud.username }}&password={{ pcloud.password }}" | jq -r '.auth')
 
 systemctl stop nginx
-systemctl stop etherpad
-systemctl stop gogs
+systemctl stop etherpad ethercalc
+systemctl stop gogs gitea
 systemctl stop php{{ php.version }}-fpm.service
 
-# Postgresql gos/redmine/nextcloud (quick phase)
+##########################################################
+# Postgres databases backup
 systemctl restart postgresql
 
 sudo su - postgres -c "pg_dump gogs > ${targetDir}/gogs-${timestamp=}.dmp"
@@ -34,55 +35,58 @@ gzip -f ${targetDir}/nextcloud-${timestamp=}.dmp
 curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}&folderid={{ pcloud.folder.id }}" -F update=@${targetDir}/nextcloud-${timestamp=}.dmp.gz
 rm -f ${targetDir}/nextcloud-${timestamp=}.dmp.gz
 
-# etherpad
 sudo su - postgres -c "pg_dump etherpad > ${targetDir}/etherpad-${timestamp=}.dmp"
 gzip -f ${targetDir}/etherpad-${timestamp=}.dmp
 curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}&folderid={{ pcloud.folder.id }}" -F update=@${targetDir}/etherpad-${timestamp=}.dmp.gz
 rm -f ${targetDir}/etherpad-${timestamp=}.dmp.gz
 systemctl start etherpad
 
+sudo su - postgres -c "pg_dump spliit > ${targetDir}/spliit-${timestamp=}.dmp"
+gzip -f ${targetDir}/spliit-${timestamp=}.dmp
+curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}&folderid={{ pcloud.folder.id }}" -F update=@${targetDir}/spliit-${timestamp=}.dmp.gz
+rm -f ${targetDir}/spliit-${timestamp=}.dmp.gz
+systemctl start spliit
+
 systemctl restart postgresql
 ##########################################################
+# Redis databases backup
+
+systemctl stop redis-server ethercalc
+cp /var/lib/redis/dump.rdb ${targetDir}/redis-${timestamp=}.rdb
+gzip -f ${targetDir}/redis-${timestamp=}.rdb
+systemctl start redis-server ethercalc
+curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}&folderid={{ pcloud.folder.id }}" -F update=@${targetDir}/redis-${timestamp=}.rdb.gz
+rm -f ${targetDir}/redis-${timestamp=}.rdb.gz
+
+##########################################################
+# Application backups
 
 # redmine
 tar zcvf ${targetDir}/redmine-${timestamp=}-files.tar.gz /data/redmine-files
+systemctl start nginx
 curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}&folderid={{ pcloud.folder.id }}" -F update=@${targetDir}/redmine-${timestamp=}-files.tar.gz
 rm -f ${targetDir}/redmine-${timestamp=}-files.tar.gz
-systemctl start nginx
 
 ## gogs
 tar zcvf ${targetDir}/gogs-${timestamp=}-data.tar.gz /data/gogs-repositories
+systemctl start gogs
 curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}&folderid={{ pcloud.folder.id }}" -F update=@${targetDir}/gogs-${timestamp=}-data.tar.gz
 rm -f ${targetDir}/gogs-${timestamp=}-data.tar.gz
-systemctl start gogs
 
 ## gitea
 tar zcvf ${targetDir}/gitea-${timestamp=}-data.tar.gz /data/gitea
+systemctl start gitea
 curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}&folderid={{ pcloud.folder.id }}" -F update=@${targetDir}/gitea-${timestamp=}-data.tar.gz
 rm -f ${targetDir}/gitea-${timestamp=}-data.tar.gz
-systemctl start gitea
 
 # nextcloud
 GZIP=-9; tar zcvf ${targetDir}/nextcloud-${timestamp=}-data.tar.gz --exclude={"nextcloud.log*"} /data/nextcloud
-curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}&folderid={{ pcloud.folder.id }}" -F update=@${targetDir}/nextcloud-${timestamp=}-data.tar.gz
-rm -f ${targetDir}/nextcloud-${timestamp=}-data.tar.gz
 systemctl start php{{ php.version }}-fpm.service
 systemctl restart nginx
+curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}&folderid={{ pcloud.folder.id }}" -F update=@${targetDir}/nextcloud-${timestamp=}-data.tar.gz
+rm -f ${targetDir}/nextcloud-${timestamp=}-data.tar.gz
 
 
-
-
-# ethercalc
-#cp /var/lib/redis/dump.rdb ${targetDir}/ethercalc-${timestamp=}.rdb
-#gzip -f ${targetDir}/ethercalc-${timestamp=}.rdb
-#curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/ethercalc-${timestamp=}.rdb.gz
-#rm -f ${targetDir}/ethercalc-${timestamp=}.rdb.gz
-#systemctl start ethercalc
-
-# picocms
-#tar zcvf ${targetDir}/picocms-${timestamp=}-data.tgz /data/picocms
-#curl -X POST "https://eapi.pcloud.com/uploadfile?auth=${auth}" -F update=@${targetDir}/picocms-${timestamp=}-data.tgz
-#rm -f ${targetDir}/picocms-${timestamp=}-data.tgz
 
 
 
